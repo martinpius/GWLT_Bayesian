7fc93aef9ca4cb428718d74ab713ab538e0d3395
Martin Pius <51404553+martinpius@users.noreply.github.com>
2024-10-04 08:39:20 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
 import __future__
 from dataclasses import dataclass 
+from typing import List
 
 @dataclass
 class TGWL_Bayesian:
+    
     model_type: str = ["nnt", "mcmc_TGWL", "nnt_real"]
+    params: str = ["alpha", "gamma","epsilon","tau"]
+    burn_in: int = 1000
+    num_iterations: int = 5000
+    samples: List[int] = [200, 1000, 10000]
+    
+    
