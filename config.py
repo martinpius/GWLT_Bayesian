b3d488d026a31c0b3da21b0c97dd4359127b221e
Martin Pius <51404553+martinpius@users.noreply.github.com>
2024-10-04 08:43:37 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from typing import List
 class TGWL_Bayesian:
     
     model_type: str = ["nnt", "mcmc_TGWL", "nnt_real"]
-    params: str = ["alpha", "gamma","epsilon","tau"]
+    params_init: float = [1e-3, 4e-3, 1e-5, 1e-8]
     burn_in: int = 1000
     num_iterations: int = 5000
     samples: List[int] = [200, 1000, 10000]
